33fca894bae4f87dfabb9cf696c072142791ba1d
KolyaBLR <bk97w@bk.ru>
2017-05-05 17:47:32 +0300

    add MyList

---
+++ SiAOD_LR2/MyList.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiAOD_LR2
+{
+    class HashList
+    {
+        public string key { get; set; }
+        public string text { get; set; }
+        public HashList Next { get; set; }
+        public HashList Back { get; set; }
+
+        public HashList() { }
+
+        public HashList(string key, string text)
+        {
+            this.key = key;
+            this.text = text;
+        }
+    }
+
+    class MyList
+    {
+        private HashList hash;
+
+        public MyList() { hash = new HashList(); }
+
+        //функция добавление нового слова
+        public void add(string key, string text)
+        {
+            /* Уходим в начало списка.
+             * Если при проверочном поиске нашли ключ то делаем перезапись.
+             * Иначе переходим в конец списка.
+             * И производим запись.
+             */
+            reverseBegin();
+            if (searchKey(key).key == key)
+            {
+                hash.key = key;
+                hash.text = text;
+            }
+            else
+            {
+                reverseEnd();
+                HashList local = new HashList(key, text);
+                local.Back = hash;
+                hash.Next = local;
+                hash = local;
+            }
+        }
+
+        //метод поиска по ключу
+        public HashList searchKey(string key)
+        {
+            /* Этот метод волшебный, и он
+             * гарантированно возвращает целый список
+             * И при вызове метода
+             * нужно всего лишь проверить совпадают ли ключи
+             */
+            reverseBegin();
+            while (hash.Next != null)
+            {
+                if (hash.key == key)
+                    return hash;
+                hash = hash.Next;
+            }
+            return hash;
+        }
+
+        //метод удаления элемента из списка
+        public void delete(string key)
+        {
+            /* Если элемент списка не является последним
+             * то вырезаем его путём создания буферной переменной
+             * иначе просто удаляем ссылки на этот последний элемент
+             */
+            searchKey(key);
+            if (hash.Next != null && hash.Back != null)
+            {
+                HashList local = hash.Next;
+                local.Back = hash.Back;
+                hash.Back.Next = local;
+                hash = local;
+            }
+            else
+            if (hash.Next == null)
+                hash.Back.Next = null;
+        }
+
+        //функция для перехода к концу списка
+        public void reverseEnd()
+        {
+            while (hash.Next != null)
+            {
+                hash = hash.Next;
+            }
+        }
+
+        //функция для перехода к началу списка
+        public void reverseBegin()
+        {
+            while (hash.Back != null)
+            {
+                hash = hash.Back;
+            }
+            if (hash.Back != null && hash.Next != null)
+                hash = hash.Next;
+        }
+    }
+}
--- SiAOD_LR2/Program.cs
+++ SiAOD_LR2/Program.cs
@@ -8,8 +8,116 @@ namespace SiAOD_LR2
 {
     class Program
     {
+        /* На основе динамических списков необходимо реализовать словарь. 
+         * Основные операции, выполняемые над данными словаря:
+         * - поиск;
+         * - вставка;
+         * - удаление.
+         */
+
+        /* Размер таблицы ркомендуется выберать по формуле 2^n.
+         * 
+         */
+
+        public static void command()
+        {
+            Console.WriteLine("Команды:");
+            Console.WriteLine("\tпоиск");
+            Console.WriteLine("\tвставка");
+            Console.WriteLine("\tудаление");
+            Console.WriteLine("\tвыход");
+        }
+
+        //функция для генерации хеш кода
+        public static int hashGeneration(string text, int count)
+        {
+            int hash = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                hash += text[i] + (int)Math.Pow(count, i);
+                hash %= count;
+            }
+            return Math.Abs(hash);
+        }
+
         static void Main(string[] args)
         {
+            int n = 0;
+            bool flag = false;
+            while (!flag)
+            {
+                try
+                {
+                    Console.Write("Введите размер хеш таблицы:");
+                    n = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+                    flag = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Не верный ввод. Повторите попытку.");
+                    flag = false;
+                }
+            }
+            MyList[] table = new MyList[n];
+            for (int i = 0; i < table.Length; i++)
+                table[i] = new MyList();
+            command();
+            while (true)
+            {
+                Console.Write("Введите команду:");
+                string consoleCommand = Convert.ToString(Console.ReadLine());
+
+                if (consoleCommand == "поиск")
+                {
+                    Console.Write("Введите ключевое слово для поиска:");
+                    string key = Convert.ToString(Console.ReadLine());
+                    HashList item = table[hashGeneration(key, n)].searchKey(key);
+                    if (item.key == key)
+                    {
+                        Console.WriteLine("key:" + item.key);
+                        Console.WriteLine("text:" + item.text);
+                    }
+                    else
+                        Console.WriteLine("По запросу ничего не найдено.");
+                }
+                else
+                if (consoleCommand == "вставка")
+                {
+                    Console.Write("Введите ключевое слово для записи:");
+                    string key = Convert.ToString(Console.ReadLine());
+                    Console.Write("Введите сопроводительный текст:");
+                    string text = Convert.ToString(Console.ReadLine());
+                    table[hashGeneration(key, n)].add(key, text);
+                    Console.WriteLine("Запись выполнена успешно.");
+                }
+                else
+                if (consoleCommand == "удаление")
+                {
+                    Console.Write("Введите ключевое слово для удаления:");
+                    string key = Convert.ToString(Console.ReadLine());
+                    MyList ho = new MyList();
+
+                    try
+                    {
+                        table[hashGeneration(key, n)].delete(key);
+                        Console.WriteLine("Удаление выполнено успешно.");
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Данный ключ не существует.");
+                    }
+
+                }
+                else
+                if (consoleCommand == "выход")
+                {
+                    break;
+                }
+                else
+                    Console.WriteLine("не верная команда. Повторите ввод.");
+            }
+            Console.Write("нажмите любую кнопку для закрытия...");
+            Console.ReadKey();
         }
     }
 }
